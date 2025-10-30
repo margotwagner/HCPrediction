46d1ad91197270010ac84df87045364cfcbf5d87
margotwagner <mwagner@salk.edu>
2025-10-30 12:37:56 -0700

    added and saved learned hidden weight matrices

--- Figure3.sh
+++ Figure3.sh
@@ -1,7 +1,6 @@
 # Multiple Conditions
 python make_figures.py --just 3 \
-  --cond_glob 
-  "./runs/ElmanRNN/shift-variants/shift-cyc/frobenius/sym*/shiftcyc_n100_fro_sym*" \
+  --cond_glob "./runs/ElmanRNN/shift-variants/shift-cyc/frobenius/sym*/shiftcyc_n100_fro_sym*" \
   "./runs/ElmanRNN/shift-variants/shift/frobenius/sym*/shift_n100_fro_sym*" \
   "./runs/ElmanRNN/mh-variants/shifted-cyc/frobenius/sym*/shiftcycmh_n100_fro_sym*" \
   "./runs/ElmanRNN/mh-variants/shifted/frobenius/sym*/shiftmh_n100_fro_sym*" \
